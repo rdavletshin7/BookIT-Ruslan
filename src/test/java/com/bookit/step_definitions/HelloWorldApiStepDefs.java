ea0c836785cdd1e4230181cc4aa9bd2d48aa7a91
Ruslan Davletshin <rdavletshin7@gmail.com>
2022-02-19 16:07:32 -0600

    Added log4j

--- src/test/java/com/bookit/step_definitions/HelloWorldApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/HelloWorldApiStepDefs.java
@@ -5,18 +5,26 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.Assert.*;
 
 public class HelloWorldApiStepDefs {
-
+    private static final Logger LOG = LogManager.getLogger();
     Response response;
 
     @Given("User sends get request to hello world api")
     public void user_sends_get_request_to_hello_world_api() {
+
+        LOG.info("Sending GET request to " +ConfigurationReader.getProperty("hello.world.api"));
+
         response = given().accept(ContentType.JSON)
                 .when().get(ConfigurationReader.getProperty("hello.world.api"));
+
+        LOG.info("Completed GET request to " +ConfigurationReader.getProperty("hello.world.api") + " with response " + response.asString());
+
     }
 
     @Then("status code is {int}")
